d77aff38bc65556ef93ad85682052b4da4fc7f3d
Roderick Tronel <trr5091a@ensfsi.onmicrosoft.com>
2024-09-19 14:57:28 +0200

    Fin class gaulois

---
+++ src/personnages/Romain.java
@@ -0,0 +1,15 @@
+package personnages;
+
+public class Romain {
+
+	public void recevoirCoup(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
