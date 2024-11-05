f294a20269c47ff89d4525cdd6910c20abe3f345
mon_user_name <roderick.tronel@uviv-tlse3.fr>
2024-11-05 15:12:44 +0100

    tp3 final !

--- src/personnages/Musee.java
+++ src/personnages/Musee.java
@@ -1,16 +1,16 @@
 package personnages;
 
 public class Musee {
-	private Equipement[] trophees = new Equipement[200];
+	private Trophee[] trophees = new Trophee[200] ;
 	private int nbTrophee = 0;
 	
 	public void donnerTrophees(Gaulois gaulois, Equipement equipement) {
 		if (nbTrophee < 200) {
-			trophees[nbTrophee] = new Trophee(gaulois, equipement).getEquipement();
-			nbTrophee++;
+			trophees[nbTrophee] = new Trophee(gaulois, equipement);
+			nbTrophee ++;	
 		}
 	}
-	
+
 	public String extraireInstructionsOCaml(){
 		String text = "let musee = [\n";
 		for (int i = 0; i < nbTrophee; i++){
